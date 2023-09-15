a04d5f9a89a15decc1fd0ae510d738e924c163b6
s1f102100793 <s1f102100793@iniad.org>
2023-09-16 03:38:52 +0900

    topページのヘッダー作成

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -1,5 +1,29 @@
+import styles from './index.module.css';
+
 const Home = () => {
-  return <></>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerTop}>
+        <div className={styles.textBottomLeft}>考える像大喜利</div>
+      </div>
+      <div className={styles.headerBottom}>
+        <div className={styles.headerBottom}>
+          <a href="/" className={styles.link}>
+            Home
+          </a>
+          <a href="/create" className={styles.link}>
+            Create
+          </a>
+          <a href="/view" className={styles.link}>
+            View
+          </a>
+          <a href="/outstanding" className={styles.link}>
+            Outstanding
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

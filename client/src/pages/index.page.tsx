167f2d7f25f3ccddf76d6d53e61dd100700649f2
s1f102100793 <s1f102100793@iniad.org>
2023-09-16 12:52:31 +0900

    Header完成

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -1,26 +1,35 @@
 import styles from './index.module.css';
 
 const Home = () => {
+  const currentPath = window.location.pathname;
+
   return (
     <div className={styles.container}>
       <div className={styles.headerTop}>
         <div className={styles.textBottomLeft}>考える像大喜利</div>
       </div>
       <div className={styles.headerBottom}>
-        <div className={styles.headerBottom}>
-          <a href="/" className={styles.link}>
-            Home
-          </a>
-          <a href="/create" className={styles.link}>
-            Create
-          </a>
-          <a href="/view" className={styles.link}>
-            View
-          </a>
-          <a href="/outstanding" className={styles.link}>
-            Outstanding
-          </a>
-        </div>
+        <a href="/" className={`${styles.link} ${currentPath === '/' ? styles.active : ''}`}>
+          Home
+        </a>
+        <a
+          href="/create"
+          className={`${styles.link} ${currentPath === '/create' ? styles.active : ''}`}
+        >
+          Create
+        </a>
+        <a
+          href="/view"
+          className={`${styles.link} ${currentPath === '/view' ? styles.active : ''}`}
+        >
+          View
+        </a>
+        <a
+          href="/outstanding"
+          className={`${styles.link} ${currentPath === '/outstanding' ? styles.active : ''}`}
+        >
+          Outstanding
+        </a>
       </div>
     </div>
   );

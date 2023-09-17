65d96d7f20cdfa97e263236d863e3c71915e76c9
s1f102100793 <s1f102100793@iniad.org>
2023-09-17 22:17:56 +0900

    Homeに持ってきた

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -1,15 +1,31 @@
+import type { BokeModel } from 'commonTypesWithClient/models';
 import Head from 'next/head';
+import { useState } from 'react';
 import Header from 'src/components/Header/Header';
+import { apiClient } from 'src/utils/apiClient';
 import styles from './index.module.css';
 
 const Home = () => {
+  const [homeBokeData, setHomeBokeData] = useState<BokeModel[]>([]);
+  const [homeBokeImg, setHomeBokeImg] = useState<string[]>([]);
+
+  const fetchHomeboke = async () => {
+    const databaseBoke = await apiClient.boke.$get();
+    if (databaseBoke) {
+      setHomeBokeData(databaseBoke);
+      setHomeBokeImg(databaseBoke.map((boke) => boke.image));
+    } else {
+      console.error('Failed to fetch boke data');
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;４00&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400&display=swap"
           rel="stylesheet"
         />
       </Head>

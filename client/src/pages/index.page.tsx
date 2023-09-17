34414b00110668ccf0ceab8216cd63d5828d6444
s1f102100793 <s1f102100793@iniad.org>
2023-09-17 23:25:51 +0900

    それっぽくなったけどcssムズイ

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -1,6 +1,6 @@
 import type { BokeModel } from 'commonTypesWithClient/models';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from 'src/components/Header/Header';
 import { apiClient } from 'src/utils/apiClient';
 import styles from './index.module.css';
@@ -8,6 +8,7 @@ import styles from './index.module.css';
 const Home = () => {
   const [homeBokeData, setHomeBokeData] = useState<BokeModel[]>([]);
   const [homeBokeImg, setHomeBokeImg] = useState<string[]>([]);
+  const [displayImages, setDisplayImages] = useState<string[]>([]);
 
   const fetchHomeboke = async () => {
     const databaseBoke = await apiClient.boke.$get();
@@ -18,6 +19,35 @@ const Home = () => {
       console.error('Failed to fetch boke data');
     }
   };
+
+  useEffect(() => {
+    fetchHomeboke();
+  }, []);
+
+  useEffect(() => {
+    if (homeBokeImg.length > 0) {
+      setDisplayImages(homeBokeImg.slice(0, 5));
+    }
+  }, [homeBokeImg]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (homeBokeImg.length === 0) return;
+
+      const newImages = [...displayImages.slice(1)];
+      const nextImageIndex = homeBokeImg.indexOf(newImages[newImages.length - 1]) + 1;
+
+      if (homeBokeImg[nextImageIndex]) {
+        newImages.push(homeBokeImg[nextImageIndex]);
+      } else {
+        newImages.push(homeBokeImg[0]);
+      }
+
+      setDisplayImages(newImages);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [displayImages, homeBokeImg]);
 
   return (
     <div className={styles.container}>
@@ -34,6 +64,16 @@ const Home = () => {
       <div className={styles.mainContent}>
         <div className={styles.mainImage}>
           <div className={styles.textContainer}>WELCOME TO RODIN OGIRI</div>
+        </div>
+        <div className={styles.imageContainer}>
+          {displayImages.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt={`Boke image ${index}`}
+              className={styles.displayedImg}
+            />
+          ))}
         </div>
       </div>
     </div>

f6754aaf0b19012ec80741c72f4e1b3567a66bff
s1f102100793 <s1f102100793@iniad.org>
2023-09-19 23:13:03 +0900

    惜しい

--- client/src/pages/view/index.page.tsx
+++ client/src/pages/view/index.page.tsx
@@ -1,7 +1,7 @@
 import type { BokeModel } from 'commonTypesWithClient/models';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect, useRef, useState } from 'react'; // useEffectをインポート
+import { useEffect, useRef, useState } from 'react';
 import Header from 'src/components/Header/Header';
 import { apiClient } from 'src/utils/apiClient';
 import styles from './view.module.css';
@@ -9,62 +9,45 @@ import styles from './view.module.css';
 const View = () => {
   const router = useRouter();
   const [bokeData, setBokeData] = useState<BokeModel[]>([]);
-
   const fetchBoke = async () => {
     const databaseBoke = await apiClient.boke.$get({});
-
     if (Array.isArray(databaseBoke)) {
       setBokeData(databaseBoke);
-    } else if (databaseBoke) {
-      console.error('Expected an array of BokeModel but received a single instance.');
-    } else {
-      console.error('Failed to fetch boke data');
     }
   };
-
   const handleBokeClick = (boke: BokeModel) => {
     router.push(`/view/${boke.bokeId}`);
   };
-
   const wrapperRef = useRef<HTMLDivElement | null>(null);
-  const [currentIndex, setCurrentIndex] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [offset, setOffset] = useState(0);
 
   useEffect(() => {
     const handleScroll = (e: WheelEvent) => {
+      e.preventDefault();
       if (wrapperRef.current) {
         const wrapperWidth = wrapperRef.current.clientWidth;
         let newOffset: number | undefined;
 
-        if (e.deltaY > 0) {
-          setCurrentIndex((prevIndex) => {
-            const nextIndex = Math.min(bokeData.length - 2, prevIndex + 1);
-            newOffset = (nextIndex - 1) * wrapperWidth * 0.33;
-            return nextIndex;
-          });
-        } else {
-          setCurrentIndex((prevIndex) => {
-            const nextIndex = Math.max(1, prevIndex - 1);
-            newOffset = (nextIndex - 1) * wrapperWidth * 0.33;
-            return nextIndex;
-          });
+        if (e.deltaY > 0 && currentIndex < bokeData.length - 1) {
+          newOffset = currentIndex * wrapperWidth * 0.33;
+          setCurrentIndex(currentIndex + 1);
+        } else if (e.deltaY < 0 && currentIndex > 0) {
+          newOffset = (currentIndex - 2) * wrapperWidth * 0.33;
+          setCurrentIndex(currentIndex - 1);
         }
-
         if (newOffset !== undefined) {
           setOffset(newOffset);
         }
       }
     };
-
-    window.addEventListener('wheel', handleScroll);
-
+    window.addEventListener('wheel', handleScroll, { passive: false });
     return () => {
       window.removeEventListener('wheel', handleScroll);
     };
-  }, [bokeData.length]);
+  }, [bokeData.length, currentIndex]);
 
   useEffect(() => {
-    console.log('useEffect');
     fetchBoke();
   }, []);
 
@@ -81,10 +64,10 @@ const View = () => {
       <Header />
       <div className={styles.contentWrapper} ref={wrapperRef}>
         <div className={styles.bokeList} style={{ transform: `translateX(-${offset}px)` }}>
-          {bokeData.slice(currentIndex - 1, currentIndex + 2).map((boke, index) => (
+          {bokeData.map((boke, index) => (
             <div
               key={boke.bokeId}
-              className={`${styles.bokeItem} ${index === 1 ? styles.centerItem : ''}`}
+              className={`${styles.bokeItem} ${index === currentIndex ? styles.centerItem : ''}`}
               onClick={() => handleBokeClick(boke)}
             >
               <div className={styles.imageBorder}>

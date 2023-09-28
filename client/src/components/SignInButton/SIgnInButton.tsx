0b6a039206d3acef0e9335a64a34cde02146201b
s1f102100793 <s1f102100793@iniad.org>
2023-09-28 18:10:35 +0900

    Googleのそれっぽいのでた

--- client/src/components/SignInButton/SIgnInButton.tsx
+++ client/src/components/SignInButton/SIgnInButton.tsx
@@ -1,18 +1,18 @@
-import { signInWithPopup } from 'firebase/auth';
-import { createAuth } from 'src/utils/firebase2';
-
-const { auth, provider } = createAuth();
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../../utils/firebaseConfig';
 
 export const SignInButton = () => {
   const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
     try {
       const result = await signInWithPopup(auth, provider);
-      console;
       const user = result.user;
       console.log(user);
     } catch (error) {
       if (error instanceof Error) {
         console.error(error.message);
+      } else {
+        console.error('An unknown error occurred');
       }
     }
   };

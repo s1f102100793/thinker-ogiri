3ab012bbc3145a0ec1c8198f27ffbd2e748b50ca
s1f102100793 <s1f102100793@iniad.org>
2023-09-28 19:15:58 +0900

    より綺麗に

--- client/src/components/SignInButton/SIgnInButton.tsx
+++ client/src/components/SignInButton/SIgnInButton.tsx
@@ -1,23 +1,45 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import type { User } from 'firebase/auth';
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 import { auth } from '../../utils/firebaseConfig';
 
 export const SignInButton = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
   const signInWithGoogle = async () => {
-    const provider = new GoogleAuthProvider();
     try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      console.log(user);
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
     } catch (error) {
-      if (error instanceof Error) {
-        console.error(error.message);
-      } else {
-        console.error('An unknown error occurred');
-      }
+      console.error(error);
     }
   };
 
-  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          {user.displayName}
+          <button onClick={() => auth.signOut()}>Sign Out</button>
+        </div>
+      ) : (
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      )}
+    </div>
+  );
 };
 
 export default SignInButton;
